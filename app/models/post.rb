818155a5eeba84f3150273422c53717ab3413624
jungyoseok <yasu3237@gmail.com>
2016-04-26 16:22:08 -0700

    reset

--- app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ActiveRecord::Base
-
-	# attr_accessible :title, :body, :category_id, :author_id
-	belongs_to :category
-end
--- app/models/section_edit.rb
+++
@@ -1,6 +0,0 @@
-class SectionEdit < ActiveRecord::Base
-
-	#belongs to editor, foreign key must be specified because this is going to be a join table
-	belongs_to :editor, :class_name => "AdminUser", :foreign_key => "admin_user_id"
-	belongs_to :section
-end
